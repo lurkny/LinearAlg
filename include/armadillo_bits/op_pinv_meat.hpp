d201e2d3867041a85a24a225096d8e56e49e0942
conrad <conrad@localhost.localdomain>
2021-03-03 16:48:45 +1000

    note on how rcond_threshold was determined

--- include/armadillo_bits/op_pinv_meat.hpp
+++ include/armadillo_bits/op_pinv_meat.hpp
@@ -75,6 +75,8 @@ op_pinv::apply_direct(Mat<typename T1::elem_type>& out, const Base<typename T1::
     out = A;
     
     const T rcond_threshold = T(1000) * std::numeric_limits<T>::epsilon();
+    // rcond_threshold was empirically determined to ensure that pinv(cov(randu(N,N)) + k*eye(N,N))
+    // obtained very similar results with and without the sympd optimisation for various values of N and k
     
     const bool status = auxlib::inv_sympd_rcond(out, rcond_threshold);
     
