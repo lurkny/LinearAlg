781cd15876cdf03b993cca773c1385ecf9fe6460
conrad <conrad@localhost.localdomain>
2021-03-31 16:42:21 +1000

    fix

--- include/armadillo_bits/newarp_SparseGenRealShiftSolve_meat.hpp
+++ include/armadillo_bits/newarp_SparseGenRealShiftSolve_meat.hpp
@@ -73,8 +73,8 @@ SparseGenRealShiftSolve<eT>::SparseGenRealShiftSolve(const SpMat<eT>& mat_obj, c
     
     if( (x_rcond < std::numeric_limits<eT>::epsilon()) || arma_isnan(x_rcond) )
       {
-      if(x_cond > eT(0))  { arma_debug_warn_level(2, "matrix is singular to working precision (rcond: ", x_rcond, ")"); }
-      else                { arma_debug_warn_level(2, "matrix is singular to working precision");                        }
+      if(x_rcond > eT(0))  { arma_debug_warn_level(2, "matrix is singular to working precision (rcond: ", x_rcond, ")"); }
+      else                 { arma_debug_warn_level(2, "matrix is singular to working precision");                        }
       return;
       }
     
