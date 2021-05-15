0894c11b33cb28f5f781885ab1a6791ee46e7b4c
conrad <conrad@localhost.localdomain>
2021-05-15 14:28:16 +1000

    mark as RC1

--- include/armadillo_bits/arma_version.hpp
+++ include/armadillo_bits/arma_version.hpp
@@ -21,8 +21,8 @@
 
 #define ARMA_VERSION_MAJOR 10
 #define ARMA_VERSION_MINOR 4
-#define ARMA_VERSION_PATCH 90
-#define ARMA_VERSION_NAME  "unstable"
+#define ARMA_VERSION_PATCH 91
+#define ARMA_VERSION_NAME  "10.5 RC1"
 
 
 
