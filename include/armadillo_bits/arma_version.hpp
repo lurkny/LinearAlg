421aae25e504e53b06949c14338d373c2225d14e
conrad <conrad@localhost.localdomain>
2021-07-07 16:58:35 +1000

    mark as RC1

--- include/armadillo_bits/arma_version.hpp
+++ include/armadillo_bits/arma_version.hpp
@@ -21,8 +21,8 @@
 
 #define ARMA_VERSION_MAJOR 10
 #define ARMA_VERSION_MINOR 5
-#define ARMA_VERSION_PATCH 90
-#define ARMA_VERSION_NAME  "experimental"
+#define ARMA_VERSION_PATCH 91
+#define ARMA_VERSION_NAME  "10.6 RC1"
 
 
 
