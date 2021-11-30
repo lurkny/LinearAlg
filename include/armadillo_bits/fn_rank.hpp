332b3cc8539efd608325793df269fca80f7b6453
conrad <conrad@localhost.localdomain>
2021-11-30 10:16:52 +1000

    optimised handling of strictly symmetric matrices

--- include/armadillo_bits/fn_rank.hpp
+++ include/armadillo_bits/fn_rank.hpp
@@ -21,6 +21,110 @@
 
 
 
+template<typename eT>
+arma_warn_unused
+inline
+typename enable_if2< is_supported_blas_type<eT>::value, uword >::result
+rank_diagmat
+  (
+  const Mat<eT>&                          A,
+        typename get_pod_type<eT>::result tol = 0.0
+  )
+  {
+  arma_extra_debug_sigprint();
+  
+  typedef typename get_pod_type<eT>::result T;
+  
+  if(A.is_empty())  { return uword(0); }
+  
+  const uword N = (std::min)(A.n_rows, A.n_cols);
+  
+  podarray<T> diag_abs_vals(N);
+  
+  T max_abs_Aii = T(0);
+  
+  for(uword i=0; i<N; ++i)
+    {
+    const eT     Aii = A.at(i,i);
+    const  T abs_Aii = std::abs(Aii);
+    
+    if(arma_isnan(Aii))  { arma_stop_runtime_error("rank(): detected NaN"); return uword(0); }
+    
+    diag_abs_vals[i] = abs_Aii;
+    
+    max_abs_Aii = (abs_Aii > max_abs_Aii) ? abs_Aii : max_abs_Aii;
+    }
+  
+  if(tol == T(0))  { tol = (std::max)(A.n_rows, A.n_cols) * max_abs_Aii * std::numeric_limits<T>::epsilon(); }
+  
+  uword count = 0;
+  
+  for(uword i=0; i<N; ++i)  { count += (diag_abs_vals[i] > tol) ? uword(1) : uword(0); }
+  
+  return count;
+  }
+
+
+
+template<typename T1>
+arma_warn_unused
+inline
+typename enable_if2< is_supported_blas_type<typename T1::elem_type>::value, uword >::result
+rank_sym
+  (
+  const Base<typename T1::elem_type,T1>& X,
+        typename T1::pod_type            tol = 0.0
+  )
+  {
+  arma_extra_debug_sigprint();
+  
+  typedef typename T1::elem_type eT;
+  typedef typename T1::pod_type   T;
+  
+  Mat<eT> A(X.get_ref());
+  
+  if(A.is_square() == false)
+    {
+    arma_stop_runtime_error("rank_sym(): given matrix must be square sized");
+    return uword(0);
+    }
+  
+  if(A.is_empty())  { return uword(0); }
+  
+  // if(is_op_diagmat<T1>::value || A.is_diagmat())
+  //   {
+  //   arma_extra_debug_print("rank(): detected diagonal matrix");
+  //   // TODO: return rank_diagmat(A, tol);
+  //   }
+  
+  Col<T> v;
+  
+  const bool status = auxlib::eig_sym(v, A);
+  
+  if(status == false)
+    {
+    arma_stop_runtime_error("rank_sym(): eig_sym failed");
+    return uword(0);
+    }
+  
+  const uword v_n_elem = v.n_elem;
+  const T*    v_mem    = v.memptr();
+  
+  // set tolerance to default if it hasn't been specified
+  if( (tol == T(0)) && (v_n_elem > 0) )
+    {
+    tol = (std::max)(A.n_rows, A.n_cols) * v_mem[v_n_elem-1] * std::numeric_limits<T>::epsilon();
+    }
+  
+  uword count = 0;
+  
+  for(uword i=0; i < v_n_elem; ++i)  { count += (v_mem[i] > tol) ? uword(1) : uword(0); }
+  
+  return count;
+  }
+
+
+
 template<typename T1>
 arma_warn_unused
 inline
@@ -46,31 +150,21 @@ rank
     {
     arma_extra_debug_print("rank(): detected diagonal matrix");
     
-    const uword N = (std::min)(A.n_rows, A.n_cols);
+    return rank_diagmat(A, tol);
+    }
+  
+  if(is_cx<eT>::no && A.is_symmetric())
+    {
+    arma_extra_debug_print("rank(): detected symmetric matrix");
     
-    podarray<T> diag_abs_vals(N);
+    return rank_sym(A, tol);
+    }
+  
+  if(is_cx<eT>::yes && A.is_hermitian())
+    {
+    arma_extra_debug_print("rank(): detected hermitian matrix");
     
-    T max_abs_Aii = T(0);
-    
-    for(uword i=0; i<N; ++i)
-      {
-      const eT     Aii = A.at(i,i);
-      const  T abs_Aii = std::abs(Aii);
-      
-      if(arma_isnan(Aii))  { arma_stop_runtime_error("rank(): detected NaN"); return uword(0); }
-      
-      diag_abs_vals[i] = abs_Aii;
-      
-      max_abs_Aii = (abs_Aii > max_abs_Aii) ? abs_Aii : max_abs_Aii;
-      }
-    
-    if(tol == T(0))  { tol = (std::max)(A.n_rows, A.n_cols) * max_abs_Aii * std::numeric_limits<T>::epsilon(); }
-    
-    uword count = 0;
-    
-    for(uword i=0; i<N; ++i)  { count += (diag_abs_vals[i] > tol) ? uword(1) : uword(0); }
-    
-    return count;
+    return rank_sym(A, tol);
     }
   
   Col<T> s;
