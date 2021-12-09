98876ab174fbc50df51aad084d3576b0699dac29
conrad <conrad@localhost.localdomain>
2021-12-09 22:42:24 +1000

    split into several functions

--- include/armadillo_bits/op_pinv_meat.hpp
+++ include/armadillo_bits/op_pinv_meat.hpp
@@ -72,39 +72,7 @@ op_pinv::apply_direct(Mat<typename T1::elem_type>& out, const Base<typename T1::
     {
     arma_extra_debug_print("op_pinv: detected diagonal matrix");
     
-    out.zeros(n_cols, n_rows);
-    
-    const uword N = (std::min)(n_rows, n_cols);
-    
-    podarray<T> diag_abs_vals(N);
-    
-    T max_abs_Aii = T(0);
-    
-    for(uword i=0; i<N; ++i)
-      {
-      const eT     Aii = A.at(i,i);
-      const  T abs_Aii = std::abs(Aii);
-      
-      if(arma_isnan(Aii))  { return false; }
-      
-      diag_abs_vals[i] = abs_Aii;
-      
-      max_abs_Aii = (abs_Aii > max_abs_Aii) ? abs_Aii : max_abs_Aii;
-      }
-    
-    if(tol == T(0))  { tol = (std::max)(n_rows, n_cols) * max_abs_Aii * std::numeric_limits<T>::epsilon(); }
-    
-    for(uword i=0; i<N; ++i)
-      {
-      if(diag_abs_vals[i] >= tol)
-        {
-        const eT Aii = A.at(i,i);
-        
-        if(Aii != eT(0))  { out.at(i,i) = eT(eT(1) / Aii); }
-        }
-      }
-    
-    return true;
+    return op_pinv::apply_diag(out, A, tol);
     }
   
   const bool expr_evaluates_to_symmat = symmat_helper::evaluates_to_symmat(expr.get_ref());
@@ -135,58 +103,7 @@ op_pinv::apply_direct(Mat<typename T1::elem_type>& out, const Base<typename T1::
     {
     arma_extra_debug_print("op_pinv: detected symmetric matrix");
     
-    Col< T> eigval;
-    Mat<eT> eigvec;
-    
-    const bool status = auxlib::eig_sym_dc(eigval, eigvec, A);
-    
-    if(status == false)  { return false; }
-    
-    if(eigval.n_elem == 0)  { out.zeros(n_cols, n_rows); return true; }
-    
-    Col<T> abs_eigval = arma::abs(eigval);
-    
-    const uvec indices = sort_index(abs_eigval, "descend");
-    
-    bool regular_indices = true;
-    
-    for(uword i=0; i < indices.n_elem; ++i)  { if(indices[i] != i)  { regular_indices = false; break; } }
-    
-    if(regular_indices == false)
-      {
-      arma_extra_debug_print("op_pinv: reordering eigenvalues");
-      
-      abs_eigval = abs_eigval.elem(indices);
-          eigval =     eigval.elem(indices);
-          eigvec =     eigvec.cols(indices);
-      }
-    
-    // set tolerance to default if it hasn't been specified
-    if(tol == T(0))  { tol = (std::max)(n_rows, n_cols) * abs_eigval[0] * std::numeric_limits<T>::epsilon(); }
-    
-    uword count = 0;
-    
-    for(uword i=0; i < abs_eigval.n_elem; ++i)  { count += (abs_eigval[i] >= tol) ? uword(1) : uword(0); }
-    
-    if(count == 0)  { out.zeros(n_cols, n_rows); return true; }
-    
-    Col<T> eigval2(count, arma_nozeros_indicator());
-    
-    uword count2 = 0;
-    
-    for(uword i=0; i < eigval.n_elem; ++i)
-      {
-      const T abs_val = abs_eigval[i];
-      const T     val =     eigval[i];
-      
-      if(abs_val >= tol)  { eigval2[count2] = (val != T(0)) ? T(T(1) / val) : T(0); ++count2; }
-      }
-    
-    const Mat<eT> eigvec_use(eigvec.memptr(), eigvec.n_rows, count, false);
-    
-    out = (eigvec_use * diagmat(eigval2)).eval() * eigvec_use.t();
-    
-    return true;
+    return op_pinv::apply_sym(out, A, tol);
     }
   
   // economical SVD decomposition 
@@ -241,6 +158,117 @@ op_pinv::apply_direct(Mat<typename T1::elem_type>& out, const Base<typename T1::
     
     out = tmp * trans(V_use);
     }
+  
+  return true;
+  }
+
+
+
+template<typename eT>
+inline
+bool
+op_pinv::apply_diag(Mat<eT>& out, const Mat<eT>& A, typename get_pod_type<eT>::result tol)
+  {
+  arma_extra_debug_sigprint();
+  
+  typedef typename get_pod_type<eT>::result T;
+  
+  out.zeros(A.n_cols, A.n_rows);
+  
+  const uword N = (std::min)(A.n_rows, A.n_cols);
+  
+  podarray<T> diag_abs_vals(N);
+  
+  T max_abs_Aii = T(0);
+  
+  for(uword i=0; i<N; ++i)
+    {
+    const eT     Aii = A.at(i,i);
+    const  T abs_Aii = std::abs(Aii);
+    
+    if(arma_isnan(Aii))  { return false; }
+    
+    diag_abs_vals[i] = abs_Aii;
+    
+    max_abs_Aii = (abs_Aii > max_abs_Aii) ? abs_Aii : max_abs_Aii;
+    }
+  
+  if(tol == T(0))  { tol = (std::max)(A.n_rows, A.n_cols) * max_abs_Aii * std::numeric_limits<T>::epsilon(); }
+  
+  for(uword i=0; i<N; ++i)
+    {
+    if(diag_abs_vals[i] >= tol)
+      {
+      const eT Aii = A.at(i,i);
+      
+      if(Aii != eT(0))  { out.at(i,i) = eT(eT(1) / Aii); }
+      }
+    }
+  
+  return true;
+  }
+
+
+
+template<typename eT>
+inline
+bool
+op_pinv::apply_sym(Mat<eT>& out, const Mat<eT>& A, typename get_pod_type<eT>::result tol)
+  {
+  arma_extra_debug_sigprint();
+  
+  typedef typename get_pod_type<eT>::result T;
+  
+  Col< T> eigval;
+  Mat<eT> eigvec;
+  
+  const bool status = auxlib::eig_sym_dc(eigval, eigvec, A);
+  
+  if(status == false)  { return false; }
+  
+  if(eigval.n_elem == 0)  { out.zeros(A.n_cols, A.n_rows); return true; }
+  
+  Col<T> abs_eigval = arma::abs(eigval);
+  
+  const uvec indices = sort_index(abs_eigval, "descend");
+  
+  bool regular_indices = true;
+  
+  for(uword i=0; i < indices.n_elem; ++i)  { if(indices[i] != i)  { regular_indices = false; break; } }
+  
+  if(regular_indices == false)
+    {
+    arma_extra_debug_print("op_pinv: reordering eigenvalues");
+    
+    abs_eigval = abs_eigval.elem(indices);
+        eigval =     eigval.elem(indices);
+        eigvec =     eigvec.cols(indices);
+    }
+  
+  // set tolerance to default if it hasn't been specified
+  if(tol == T(0))  { tol = (std::max)(A.n_rows, A.n_cols) * abs_eigval[0] * std::numeric_limits<T>::epsilon(); }
+  
+  uword count = 0;
+  
+  for(uword i=0; i < abs_eigval.n_elem; ++i)  { count += (abs_eigval[i] >= tol) ? uword(1) : uword(0); }
+  
+  if(count == 0)  { out.zeros(A.n_cols, A.n_rows); return true; }
+  
+  Col<T> eigval2(count, arma_nozeros_indicator());
+  
+  uword count2 = 0;
+  
+  for(uword i=0; i < eigval.n_elem; ++i)
+    {
+    const T abs_val = abs_eigval[i];
+    const T     val =     eigval[i];
+    
+    if(abs_val >= tol)  { eigval2[count2] = (val != T(0)) ? T(T(1) / val) : T(0); ++count2; }
+    }
+  
+  const Mat<eT> eigvec_use(eigvec.memptr(), eigvec.n_rows, count, false);
+  
+  out = (eigvec_use * diagmat(eigval2)).eval() * eigvec_use.t();
   
   return true;
   }
