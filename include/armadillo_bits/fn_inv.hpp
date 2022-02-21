63f27093adb8f5f0369019e6bd40a20e7529851d
conrad <conrad@localhost.localdomain>
2022-02-21 12:04:10 +1000

    fix

--- include/armadillo_bits/fn_inv.hpp
+++ include/armadillo_bits/fn_inv.hpp
@@ -150,7 +150,7 @@ inv_sympd
 template<typename T1>
 arma_warn_unused
 arma_inline
-typename enable_if2< is_supported_blas_type<typename T1::elem_type>::value, const Op<T1, op_inv_spd_default> >::result
+typename enable_if2< is_supported_blas_type<typename T1::elem_type>::value, const Op<T1, op_inv_spd> >::result
 inv_sympd
   (
   const Base<typename T1::elem_type, T1>& X,
