fa73a168700865993494b7950b068acf4b5c220a
conrad <conrad@localhost.localdomain>
2022-03-28 17:00:25 +1000

    fix

--- include/armadillo_bits/arma_config.hpp
+++ include/armadillo_bits/arma_config.hpp
@@ -86,6 +86,13 @@ struct arma_config
   #endif
   
   
+  #if defined(ARMA_USE_ATLAS)
+    static constexpr bool atlas = true;
+  #else
+    static constexpr bool atlas = false;
+  #endif
+  
+  
   #if defined(ARMA_USE_NEWARP)
     static constexpr bool newarp = true;
   #else
