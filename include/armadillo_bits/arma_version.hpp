b13976c6f65789142136749e83464edceb3365c8
conrad <conrad@localhost.localdomain>
2022-05-25 13:30:54 +1000

    mark as experimental

--- include/armadillo_bits/arma_version.hpp
+++ include/armadillo_bits/arma_version.hpp
@@ -23,8 +23,8 @@
 
 #define ARMA_VERSION_MAJOR 11
 #define ARMA_VERSION_MINOR 1
-#define ARMA_VERSION_PATCH 1
-#define ARMA_VERSION_NAME  "Angry Kitchen Appliance"
+#define ARMA_VERSION_PATCH 90
+#define ARMA_VERSION_NAME  "experimental"
 
 
 
