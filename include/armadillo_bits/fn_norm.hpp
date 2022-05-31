20d33c2656df0a1db4e0a6e807218c0120ec7be0
conrad <conrad@localhost.localdomain>
2022-05-31 14:14:12 +1000

    avoid copy for SpSubview_col

--- include/armadillo_bits/fn_norm.hpp
+++ include/armadillo_bits/fn_norm.hpp
@@ -185,6 +185,29 @@ norm
   typedef typename T1::elem_type eT;
   typedef typename T1::pod_type   T;
   
+  if(is_SpSubview_col<T1>::value)
+    {
+    const SpSubview_col<eT>& sv = reinterpret_cast< const SpSubview_col<eT>& >(expr);
+    
+    if(sv.n_rows == sv.m.n_rows)
+      {
+      const SpMat<eT>& m   = sv.m;
+      const uword      col = sv.aux_col1;
+      
+      // create a fake dense vector to allow reuse of code for dense vectors
+      Col<eT> fake_vector( access::rwp(&(m.values[ m.col_ptrs[col] ])), sv.n_nonzero, false );
+      
+      const Proxy< Col<eT> > P_fake_vector(fake_vector);
+      
+      if(k == uword(1))  { return op_norm::vec_norm_1(P_fake_vector); }
+      if(k == uword(2))  { return op_norm::vec_norm_2(P_fake_vector); }
+      
+      arma_debug_check( (k == 0), "norm(): k must be greater than zero" );
+      
+      return op_norm::vec_norm_k(P_fake_vector, int(k));
+      }
+    }
+  
   const unwrap_spmat<T1> U(expr);
   const SpMat<eT>& X   = U.M;
   
