603734465dd0951771e2c89f45daa888c2d0aa07
conrad <conrad@localhost.localdomain>
2022-07-02 16:06:23 +1000

    fix: zero out accumulation arrays at start of each iteration

--- include/armadillo_bits/gmm_diag_meat.hpp
+++ include/armadillo_bits/gmm_diag_meat.hpp
@@ -2159,6 +2159,9 @@ gmm_diag<eT>::km_iterate(const Mat<eT>& X, const uword max_iter, const bool verb
       }
     #else
       {
+      acc_hefts.zeros();
+      acc_means.zeros();
+      
       uword* acc_hefts_mem = acc_hefts.memptr();
       uword* last_indx_mem = last_indx.memptr();
       
