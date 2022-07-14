eae8dea3dd081d91482192ce71988507fff46165
conrad <conrad@localhost.localdomain>
2022-07-14 15:39:56 +1000

    mark use of operator<< as deprecated

--- include/armadillo_bits/Mat_meat.hpp
+++ include/armadillo_bits/Mat_meat.hpp
@@ -2973,7 +2973,7 @@ Mat<eT>::operator/=(const spdiagview<eT>& X)
 
 
 template<typename eT>
-arma_cold
+arma_deprecated
 inline
 mat_injector< Mat<eT> >
 Mat<eT>::operator<<(const eT val)
@@ -2984,7 +2984,7 @@ Mat<eT>::operator<<(const eT val)
 
 
 template<typename eT>
-arma_cold
+arma_deprecated
 inline
 mat_injector< Mat<eT> >
 Mat<eT>::operator<<(const injector_end_of_row<>& x)
