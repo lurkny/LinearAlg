0ca754e7d0419dbb5927496f99002eea5d1d1315
conrad <conrad@localhost.localdomain>
2022-09-16 12:13:28 +1000

    simplifications

--- include/armadillo_bits/sp_auxlib_meat.hpp
+++ include/armadillo_bits/sp_auxlib_meat.hpp
@@ -1311,17 +1311,10 @@ sp_auxlib::spsolve_refine(Mat<typename T1::elem_type>& X, typename T1::pod_type&
     
     const Mat<eT>& B = (B_is_modified) ?  B_copy : B_unwrap;
     
-    if(A.n_rows > A.n_cols)
+    if( (A.n_rows > A.n_cols) || (A.n_rows < A.n_cols) )
       {
-      arma_stop_logic_error("spsolve(): solving over-determined systems currently not supported");
       X.soft_reset();
-      return false;
-      }
-    else
-    if(A.n_rows < A.n_cols)
-      {
-      arma_stop_logic_error("spsolve(): solving under-determined systems currently not supported");
-      X.soft_reset();
+      arma_stop_logic_error("spsolve(): solving under-determined / over-determined systems is currently not supported");
       return false;
       }
     
