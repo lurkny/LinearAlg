b99aa333308cdbe14cec3803d5256c093d86d29c
conrad <conrad@localhost.localdomain>
2023-02-20 13:00:08 +1000

    version bump

--- include/armadillo_bits/arma_version.hpp
+++ include/armadillo_bits/arma_version.hpp
@@ -21,10 +21,10 @@
 
 
 
-#define ARMA_VERSION_MAJOR 11
-#define ARMA_VERSION_MINOR 5
-#define ARMA_VERSION_PATCH 90
-#define ARMA_VERSION_NAME  "unstable"
+#define ARMA_VERSION_MAJOR 12
+#define ARMA_VERSION_MINOR 0
+#define ARMA_VERSION_PATCH 0
+#define ARMA_VERSION_NAME  "Cortisol Profusion"
 
 
 
